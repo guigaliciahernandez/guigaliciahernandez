c71eb1cc624a9df48f169e44d796703782a019ba
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 13:14:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-let scene, camera, renderer;
+document.addEventListener('DOMContentLoaded', function() {
+    const circle = document.querySelector('.circle');
+    const text = document.querySelector('.text');
 
-// Initialization
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-  
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('scene-container').appendChild(renderer.domElement);
-  
-    // Add a sphere
-    let geometry = new THREE.SphereGeometry(1, 32, 32);
-    let material = new THREE.MeshBasicMaterial({ color: 0xFF0000 }); // basic red color for visibility
-    let sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-  
-    // Render
-    renderer.render(scene, camera);
-}
-
-// Resize Listener
-window.addEventListener('resize', function() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+    circle.addEventListener('click', function() {
+        text.style.opacity = '1';
+    });
 });
-
-init();
 
 
