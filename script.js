ea3bf8c7e3659eb2734e3de4e7770d8f07f880eb
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 16:55:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,76 @@
-let scene, camera, renderer, spheres = [];
+// Scene
+const scene = new THREE.Scene();
 
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 50;
 
-    // Multiple Spheres with varying opacity
-    const radii = [4.8, 5, 5.2];
-    for (let i = 0; i < radii.length; i++) {
-        const geometry = new THREE.SphereGeometry(radii[i], 64, 64);
-        const material = new THREE.MeshPhongMaterial({
-            color: 0x8844AA,
-            emissive: 0x221122,
-            shininess: 10,
-            transparent: true,
-            opacity: (i === 1) ? 0.5 : 0.2 // Central sphere more opaque
-        });
-        const sphere = new THREE.Mesh(geometry, material);
-        spheres.push(sphere);
-        scene.add(sphere);
-    }
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
 
-    // Ambient Light
-    const ambient = new THREE.AmbientLight(0x222222);
-    scene.add(ambient);
+// OrbitControls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    // Point light at center for glowing effect
-    const pointLight = new THREE.PointLight(0xffffff, 1, 15);
-    scene.add(pointLight);
+// Lights
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
 
-    camera.position.z = 15;
+const spotlight = new THREE.SpotLight(0xffffff);
+spotlight.position.set(15, 40, 35);
+spotlight.castShadow = true;
+scene.add(spotlight);
 
-    animate();
+// Spheres
+const spheres = [];
+const sphereMaterial = new THREE.MeshPhongMaterial();
+for (let i = 0; i < 100; i++) {
+    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+    sphereMaterial.color = new THREE.Color(`hsl(${Math.random() * 290 + 250}, 100%, 70%)`);
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.castShadow = true;
+    sphere.position.set(
+        (Math.random() - 0.5) * 100,
+        (Math.random() - 0.5) * 100,
+        (Math.random() - 0.5) * 100
+    );
+    spheres.push(sphere);
+    scene.add(sphere);
 }
 
+// Random direction array
+const directions = [];
+for (let i = 0; i < spheres.length; i++) {
+    directions.push(new THREE.Vector3((Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2));
+}
+
+// Animate
 function animate() {
     requestAnimationFrame(animate);
-    for (let sphere of spheres) {
-        sphere.rotation.x += 0.002;
-        sphere.rotation.y += 0.002;
-    }
+
+    // Move spheres
+    spheres.forEach((sphere, index) => {
+        sphere.position.add(directions[index].multiplyScalar(0.05));
+
+        // Reverse direction if out of bounds
+        if (sphere.position.x > 50 || sphere.position.x < -50) directions[index].x *= -1;
+        if (sphere.position.y > 50 || sphere.position.y < -50) directions[index].y *= -1;
+        if (sphere.position.z > 50 || sphere.position.z < -50) directions[index].z *= -1;
+    });
+
+    controls.update();
     renderer.render(scene, camera);
 }
 
-window.addEventListener('resize', function() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
+// Handle window resize
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-init();
+animate();
+
