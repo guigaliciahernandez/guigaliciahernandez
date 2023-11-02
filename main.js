66723e5ab307d05e361e1d3d233515a3f51f684c
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 13:11:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,27 @@
-let scene, camera, renderer, controls, spheres = [];
+let scene, camera, renderer;
 
-// Scene & Camera
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Renderer
-renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true }); // added alpha: true
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('scene-container').appendChild(renderer.domElement);
-
-// Add an ambient light
-let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);  // soft white light
-scene.add(ambientLight);
-
-// Add Spheres with Gradient
-let geometry = new THREE.SphereGeometry(1, 32, 32);
-let materials = [
-    new THREE.MeshBasicMaterial({ color: 0x8E2DE2 }),
-    new THREE.MeshBasicMaterial({ color: 0x4A00E0 })
-];
-
-for(let i=0; i<2; i++) {
-    let sphere = new THREE.Mesh(geometry, materials[i]);
-    sphere.position.y = i * 2.5;
+// Initialization
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+  
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('scene-container').appendChild(renderer.domElement);
+  
+    // Add a sphere
+    let geometry = new THREE.SphereGeometry(1, 32, 32);
+    let material = new THREE.MeshBasicMaterial({ color: 0xFF0000 }); // basic red color for visibility
+    let sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
-    spheres.push(sphere);
-}
-
-// Orbit Controls
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-// Animation
-function animate() {
-    requestAnimationFrame(animate);
-
-    // Rotate Spheres
-    spheres.forEach(sphere => {
-        sphere.rotation.x += 0.005;
-        sphere.rotation.y += 0.005;
-    });
-
+  
+    // Render
     renderer.render(scene, camera);
 }
 
-// Window Resize
-window.addEventListener('resize', () => {
+// Resize Listener
+window.addEventListener('resize', function() {
     let width = window.innerWidth;
     let height = window.innerHeight;
     renderer.setSize(width, height);
@@ -53,7 +29,6 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-console.log("Script Loaded and Running");  // Logging
+init();
 
-animate();
 
