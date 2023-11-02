7293f1bf50ac7fe213bf0650805230c070484b73
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 00:04:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,83 @@
 let words = ['Happy', 'Joy', 'Laugh', 'Cheerful', 'Delight'];
-let colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F1', '#33FFF1'];
+let colors = [{
+    background: '#FF5733',
+    text: 'white',
+    font: 'Arial, sans-serif'
+  },
+  {
+    background: '#33FF57',
+    text: 'white',
+    font: 'Arial, sans-serif'
+  },
+  // ... add more colors similarly
+];
+let colorIndex = 0;
 
-document.getElementById('smileWord').addEventListener('click', function() {
-    let randomIndex = Math.floor(Math.random() * words.length);
-    this.innerHTML = words[randomIndex];
-    document.body.style.backgroundColor = colors[randomIndex];
-    animateColorDrop();
+document.getElementById('smileWord').addEventListener('click', function(event) {
+  let randomIndex = Math.floor(Math.random() * words.length);
+  this.innerHTML = words[randomIndex];
+  animateColorDrop(event.clientX, event.clientY);
 });
 
-function animateColorDrop() {
-    const circle = document.createElement("div");
-    circle.classList.add("circle-animation");
-    circle.style.backgroundColor = "white"; // Default circle color
-    document.body.appendChild(circle);
+function animateColorDrop(x, y) {
+  const circle = document.createElement("div");
+  circle.style.position = "fixed";
+  circle.style.width = "0";
+  circle.style.height = "0";
+  circle.style.borderRadius = "50%";
+  circle.style.zIndex = "-1";
+  document.body.appendChild(circle);
 
-    const x = window.innerWidth / 2;
-    const y = window.innerHeight / 2;
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
 
-    circle.style.left = x + "px";
-    circle.style.top = y + "px";
+  const maxDimension = Math.max(document.documentElement.clientWidth, document.documentElement.clientHeight);
+  const animationDuration = 1200;
 
-    const maxDimension = Math.max(document.documentElement.clientWidth, document.documentElement.clientHeight);
-    const animationDuration = 1200;
-
-    anime({
-        targets: circle,
-        width: [0, maxDimension * 2],
-        height: [0, maxDimension * 2],
-        left: [x + "px", x - maxDimension + "px"],
-        top: [y + "px", y - maxDimension + "px"],
-        duration: animationDuration,
-        easing: "easeInOutQuad",
-        complete: () => {
-            document.body.removeChild(circle);
+  anime({
+    targets: circle,
+    width: [0, maxDimension * 2],
+    height: [0, maxDimension * 2],
+    left: [x + "px", x - maxDimension + "px"],
+    top: [y + "px", y - maxDimension + "px"],
+    backgroundColor: [
+      colors[colorIndex].background,
+      colors[colorIndex].background
+    ],
+    boxShadow: [
+      "0 0 100px 30px " + colors[colorIndex].text,
+      "0 0 100px 30px " + colors[colorIndex].text
+    ],
+    duration: animationDuration,
+    easing: "easeInOutQuad",
+    begin: () => {
+      document.body.style.color = colors[colorIndex].text;
+      circle.style.backgroundColor = colors[colorIndex].background;
+      circle.style.boxShadow = "0 0 100px 30px " + colors[colorIndex].text;
+    },
+    update: (anim) => {
+      const circleRect = circle.getBoundingClientRect();
+      const textElements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li, a, .separator");
+      textElements.forEach(element => {
+        const elementRect = element.getBoundingClientRect();
+        if (rectsIntersect(circleRect, elementRect)) {
+          element.style.color = colors[colorIndex].text;
+          element.style.fontFamily = colors[colorIndex].font;
         }
-    });
+      });
+    },
+    complete: () => {
+      document.body.style.backgroundColor = colors[colorIndex].background;
+      document.body.style.fontFamily = colors[colorIndex].font;
+      colorIndex = (colorIndex + 1) % colors.length;
+      document.body.removeChild(circle);
+    },
+  });
+}
+
+function rectsIntersect(rect1, rect2) {
+  return !(rect2.left > rect1.right ||
+    rect2.right < rect1.left ||
+    rect2.top > rect1.bottom ||
+    rect2.bottom < rect1.top);
 }
