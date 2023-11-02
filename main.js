d45710193cbfb5f609cb0fffda764e6edd465182
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 15:45:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,81 @@
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, spheres, raycaster, mouse;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+init();
+animate();
 
-let geometry = new THREE.SphereGeometry(1, 32, 32);
-let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-let sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-camera.position.z = 3;
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+
+    spheres = [];
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const vertexShader = `
+        varying vec3 vUv;
+        void main() {
+            vUv = position; 
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+        }`;
+    const fragmentShader = `
+        varying vec3 vUv;
+        void main() {
+            float gradient = dot(normalize(vUv), vec3(0.0, 0.0, 1.0));
+            gl_FragColor = vec4(vec3(0.5 + 0.5 * gradient), 0.6);
+        }`;
+    const material = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        blending: THREE.AdditiveBlending,
+        side: THREE.BackSide,
+        transparent: true
+    });
+
+    for (let i = 0; i < 5; i++) {
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.scale.set(1 + i * 0.5, 1 + i * 0.5, 1 + i * 0.5);
+        spheres.push(sphere);
+        scene.add(sphere);
+    }
+
+    camera.position.z = 6;
+
+    window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('click', onClick, false);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(spheres);
+
+    if (intersects.length > 0) {
+        gsap.to(intersects[0].object.scale, {
+            x: intersects[0].object.scale.x + 0.5,
+            y: intersects[0].object.scale.y + 0.5,
+            z: intersects[0].object.scale.z + 0.5,
+            duration: 0.5,
+            yoyo: true,
+            repeat: 1
+        });
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
-
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 }
-animate();
-
