e76caa64dc4d7dbc4e5ac92c1a31cef15aaf88ea
guigaliciahernandez <91439030+guigaliciahernandez@users.noreply.github.com>
2023-11-02 00:57:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 let words = ['EMBRACE', 'CHANGE', 'ENJOY THE RIDE'];
 let scaleFactors = [1, 2, 3];
-
-let colorSchemes = [
-    {background: '#FF5733', text: 'white'},
-    {background: '#33FF57', text: 'black'},
-    {background: '#5733FF', text: 'white'}
-];
-
-let colorIndex = 0;
+let currentWordIndex = 0;
 
 document.getElementById('word').addEventListener('click', function() {
-    colorIndex = (colorIndex + 1) % words.length;
-    this.innerHTML = words[colorIndex];
-    this.style.fontSize = `${50 * scaleFactors[colorIndex]}px`;
+    // Increment the index for the next word
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+    
+    // Set the word and its size
+    this.innerHTML = words[currentWordIndex];
+    this.style.fontSize = `${50 * scaleFactors[currentWordIndex]}px`;
+    
+    // Call the animation
     animateColorDrop(window.innerWidth / 2, window.innerHeight / 2);
 });
 
 function animateColorDrop(x, y) {
     const circle = document.createElement("div");
     circle.classList.add("circle-animation");
-    circle.style.backgroundColor = colorSchemes[colorIndex].background;
     document.body.appendChild(circle);
 
     const maxDimension = Math.max(document.documentElement.clientWidth, document.documentElement.clientHeight);
@@ -33,12 +30,7 @@ function animateColorDrop(x, y) {
         top: [y + "px", y - maxDimension + "px"],
         duration: animationDuration,
         easing: "easeInOutQuad",
-        begin: () => {
-            document.body.style.color = colorSchemes[colorIndex].text;
-            circle.style.backgroundColor = colorSchemes[colorIndex].background;
-        },
         complete: () => {
-            document.body.style.backgroundColor = colorSchemes[colorIndex].background;
             document.body.removeChild(circle);
         }
     });
@@ -47,11 +39,4 @@ function animateColorDrop(x, y) {
 setInterval(() => {
     animateColorDrop();
 }, 2000);
-
-
-// Animation on Body Click
-document.body.addEventListener("click", (event) => {
-    animateColorDrop(event.clientX, event.clientY);
-});
-
 
